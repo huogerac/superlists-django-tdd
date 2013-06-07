8a3ce649f8e10fd147a4c944fc70ad461f4a64ce
Roger Camargo <huogerac@gmail.com>
2013-06-07 18:04:58 +1000

    Functional test using model Item

--- functional_tests.py
+++ functional_tests.py
@@ -40,13 +40,21 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #import time
         #time.sleep(10)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        
-        
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')        
     
 
 if __name__ == '__main__':
